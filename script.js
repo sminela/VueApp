b7003740c44999e4edf17c058f55fa031830ce05
sminela <salkicevic.minela1@gmail.com>
2020-03-05 19:57:06 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+var vm = new Vue ({
+    el: '#app',
+    data: {
+        person: {
+            firstName: '',
+            lastName: '',
+            position: '',
+            phoneNum: '',
+            email: '',
+            address: '',
+            linkedin: '',
+            education: '',
+            profile: '',
+            experience: '',
+            expertise: ''
+        },
+        show: true
+    }
+});
